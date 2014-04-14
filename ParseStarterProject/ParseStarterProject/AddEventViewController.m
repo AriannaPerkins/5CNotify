1496c1437eb2635cba203c5d6129fa95ccc23748
AriannaPerkins <cperkins@hmc.edu>
2014-04-14 03:15:38 -0700

    started code for pfuser’s eventsCreated, eventsAttending (just added comments, no functionality changes)

--- ParseStarterProject/ParseStarterProject/AddEventViewController.m
+++ ParseStarterProject/ParseStarterProject/AddEventViewController.m
@@ -807,8 +807,15 @@ UILabel* asteriskMessage;
         newEvent[@"locationText"] = self.locationField.text;
         newEvent[@"description"] = self.descriptionView.text;
         newEvent[@"openTo"] = openToSwitches;
-        newEvent[@"rsvpCount"] = @0; // When created, rsvp count is 0
+        newEvent[@"rsvpCount"] = @1; // When created, rsvp count is 0
         [newEvent saveInBackground];
+
+//        // Add the event's objectId to the fields in the current PFUser which are (will be)
+//        // NSMutableArray* eventsCreated and NSMutableArray* eventsAttending.
+//        // Note: These need to be initialized somewhere.
+//        PFUser *curr = [PFUser currentUser];
+//        [curr[@"eventsCreated"] addObject:newEvent.objectId];
+//        [curr[@"eventsAttending"] addObject:newEvent.objectId];
         
         //Reset all values of text fields
         _addEventField.text = nil;
