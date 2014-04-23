d2134b195f1949fdb860e10406af0c6e6540d1cb
p-garratt <paigegarratt@dhcp-59-102.st.hmc.edu>
2014-04-22 22:01:00 -0700

    Added test target and some sample tests

---
+++ ParseStarterProject/5CNotify Tests/TableViewTests.h
@@ -0,0 +1,13 @@
+//
+//  TableViewTests.h
+//  5CNotify
+//
+//  Created by Paige Garratt on 4/22/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableViewTests : XCTestCase
+
+@end
---
+++ ParseStarterProject/5CNotify Tests/_CNotify_Tests.m
@@ -0,0 +1,34 @@
+//
+//  _CNotify_Tests.m
+//  5CNotify Tests
+//
+//  Created by Paige Garratt on 4/22/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _CNotify_Tests : XCTestCase
+
+@end
+
+@implementation _CNotify_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    //XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
