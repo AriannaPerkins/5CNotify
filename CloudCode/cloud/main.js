4a3390d47e0d8977c8a5fb484e20c75d4a83efef
p-garratt <paigegarratt@wl-222-13.wpa.claremont.edu>
2014-04-26 15:29:29 -0700

    Worked on autoscrolling, but it still looks funky. We should talk about this more.

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -2,7 +2,7 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.job("eventDeletion", function(request, status) {
-  // Set up to modify user data
+  // Query for user events that end earlier than the current time.
   Parse.Cloud.useMasterKey();
   var UserEvents = Parse.Object.extend("UserEvents");
   var query = new Parse.Query(UserEvents);
@@ -11,7 +11,7 @@ Parse.Cloud.job("eventDeletion", function(request, status) {
   query.find( {
       success: function(results) {
         alert("Successfully retrieved " + results.length + " events");
-        // Do something with the returned Parse.Object values
+        //Delete all events that have already occured
         for (var i = 0; i < results.length; i++) { 
             var object = results[i];
             object.destroy({
@@ -26,6 +26,61 @@ Parse.Cloud.job("eventDeletion", function(request, status) {
     },
     error: function(error) {
         console.log("Object retreival failed with error: " + error.code + " " + error.message);
+    }
+  });
+});
+
+Parse.Cloud.job("userEventArrayCleanup", function(request, status){
+  Parse.Cloud.useMasterKey();
+  // Query for all users
+  var userQuery = new Parse.Query(Parse.User);
+  //Query for all events
+  var userEvents = Parse.Object.extend("UserEvents");
+  var eventQuery = new Parse.Query(userEvents);
+
+  //Go through each user and remove events that no longer exist
+  // in both eventsAttending and eventsCreated
+  userQuery.limit(1000);
+  userQuery.find( {
+    success: function(users){
+      for (var i = 0; i < users.length; i++) {
+        var user = users[i];
+        var eventsAttending = new Array( user.get("eventsAttending"));
+        for (var i = 0; i < eventsAttending.length; i++) {
+          var objectID = eventsAttending[i];
+          eventQuery.get(objectID, {
+          success: function(object) {
+            // The object was retrieved successfully. Do nothing
+          },
+          error: function(object, error) {
+            //Object does not exist, must be deleted
+            if (error.code == "101"){
+              user.remove("eventsAttending", objectID);
+              console.log("Object deleted");
+            } 
+          }
+          });
+        };
+        var eventsCreated = new Array(user.get("eventsCreated"));
+        for (var i = 0; i < eventsCreated.length; i++) {
+          var objectID = eventsCreated[i];
+          eventQuery.get(objectID, {
+          success: function(object) {
+            // The object was retrieved successfully. Do nothing
+          },
+          error: function(object, error) {
+            //Object does not exist, must be deleted
+            if (error.code == "101"){
+              user.remove("eventsCreated", objectID);
+              console.log("Object deleted");
+            } 
+          }
+          });
+        };
+      }
+    },
+    error: function(object, error){
+      console.log("Object retreival failed with error: " + error.code + " " + error.message);
     }
   });
 });
